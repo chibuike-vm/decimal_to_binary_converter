993765cf02d6e73f8395754c3e4f880e4abf890d
Chibuike Mba <chibuike.code@gmail.com>
2023-01-23 15:05:51 +0100

    Add binConv.c

---
+++ bin_main/binConv.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../bin_header/binConv.h"
+
+/**
+ * main - Program entry point.
+ *
+ * Description: Calls the deciToBinConv() function to carry out its designated
+ * task of converting unsigned decimal integers to binary.
+ *
+ * Return: Returns zero as an exit status upon successful execution.
+ */
+int main(void)
+{
+	deciToBinConv();
+
+	return (0);
+}
+
